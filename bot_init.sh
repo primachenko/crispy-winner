d3dbd119e05485aae9933624b173891c58708c5e
Oleg Primachenko <primachenko_oleg@outlook.com>
2024-02-18 08:47:08 +0700

    remove generation bot-id

--- bot_init.sh
+++ bot_init.sh
@@ -3,6 +3,5 @@ source config
 read -p "Enter bot token: " token
 read -p "Enter techsupport chat-id: " chat_id
 
-echo "ID=${token%:*}" > $BOT_CONFIG
 echo "SECRET=\"$token\"" >> $BOT_CONFIG
 echo "CHAT_ID=$chat_id" >> $BOT_CONFIG
