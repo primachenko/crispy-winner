ee6f2189192f4106d17760de9640787d15f011d6
Oleg Primachenko <primachenko_oleg@outlook.com>
2024-02-18 16:37:33 +0700

    add mgmt chat-id

--- bot_init.sh
+++ bot_init.sh
@@ -1,7 +1,9 @@
 source config
 
-read -p "Enter bot token: " token
-read -p "Enter techsupport chat-id: " chat_id
+read -p "Enter bot token: " bot_token
+read -p "Enter techsupport chat-id: " techsupport_chat_id
+read -p "Enter mgmt chat-id: " mgmt_chat_id
 
-echo "SECRET=\"$token\"" >> $BOT_CONFIG
-echo "CHAT_ID=$chat_id" >> $BOT_CONFIG
+echo "SECRET=\"$bot_token\"" >> $BOT_CONFIG
+echo "CHAT_ID=$techsupport_chat_id" >> $BOT_CONFIG
+echo "MGMT_CHAT_ID=$mgmt_chat_id" >> $BOT_CONFIG
