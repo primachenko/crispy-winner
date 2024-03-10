f3f623b7127bc0b0c3b189929e1c954aa8ac16de
Oleg Primachenko <primachenko_oleg@outlook.com>
2024-03-10 15:10:49 +0700

    add qnap support

--- bot_init.sh
+++ bot_init.sh
@@ -4,6 +4,16 @@ read -p "Enter bot token: " bot_token
 read -p "Enter techsupport chat-id: " techsupport_chat_id
 read -p "Enter mgmt chat-id: " mgmt_chat_id
 
+read -p "Enter QNAP URL: " qnap_url
+read -p "Enter QNAP port: " qnap_port
+read -p "Enter QNAP user: " qnap_user
+read -p "Enter QNAP password: " qnap_password
+
 echo "SECRET=\"$bot_token\"" >> $BOT_CONFIG
 echo "CHAT_ID=$techsupport_chat_id" >> $BOT_CONFIG
 echo "MGMT_CHAT_ID=$mgmt_chat_id" >> $BOT_CONFIG
+
+echo "QTS_HOST=\"$qnap_url\"" >> $BOT_CONFIG
+echo "QTS_PORT=$qnap_port" >> $BOT_CONFIG
+echo "QTS_USER=\"$qnap_user\"" >> $BOT_CONFIG
+echo "QTS_PASSWORD=\"$qnap_password\"" >> $BOT_CONFIG
