d750ec54c9aeaa82487ca2ff299eebdb08518729
Oleg Primachenko <primachenko_oleg@outlook.com>
2024-03-30 17:15:35 +0700

    support google tables integration

--- bot_init.sh
+++ bot_init.sh
@@ -9,6 +9,8 @@ read -p "Enter QNAP port: " qnap_port
 read -p "Enter QNAP user: " qnap_user
 read -p "Enter QNAP password: " qnap_password
 
+read -p "Enter Google Tables table name: " google_table_name
+
 echo "SECRET=\"$bot_token\"" >> $BOT_CONFIG
 echo "CHAT_ID=$techsupport_chat_id" >> $BOT_CONFIG
 echo "MGMT_CHAT_ID=$mgmt_chat_id" >> $BOT_CONFIG
@@ -17,3 +19,5 @@ echo "QTS_HOST=\"$qnap_url\"" >> $BOT_CONFIG
 echo "QTS_PORT=$qnap_port" >> $BOT_CONFIG
 echo "QTS_USER=\"$qnap_user\"" >> $BOT_CONFIG
 echo "QTS_PASSWORD=\"$qnap_password\"" >> $BOT_CONFIG
+
+echo "GOOGLE_TABLE_NAME=\"$google_table_name\"" >> $BOT_CONFIG
