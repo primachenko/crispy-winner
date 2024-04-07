867a4d3238a27bbd5f0d487e852918e631b465f1
Oleg Primachenko <primachenko_oleg@outlook.com>
2024-04-07 14:21:09 +0700

    remove qnap port

--- bot_init.sh
+++ bot_init.sh
@@ -5,7 +5,6 @@ read -p "Enter techsupport chat-id: " techsupport_chat_id
 read -p "Enter mgmt chat-id: " mgmt_chat_id
 
 read -p "Enter QNAP URL: " qnap_url
-read -p "Enter QNAP port: " qnap_port
 read -p "Enter QNAP user: " qnap_user
 read -p "Enter QNAP password: " qnap_password
 
@@ -16,7 +15,6 @@ echo "CHAT_ID=$techsupport_chat_id" >> $BOT_CONFIG
 echo "MGMT_CHAT_ID=$mgmt_chat_id" >> $BOT_CONFIG
 
 echo "QTS_HOST=\"$qnap_url\"" >> $BOT_CONFIG
-echo "QTS_PORT=$qnap_port" >> $BOT_CONFIG
 echo "QTS_USER=\"$qnap_user\"" >> $BOT_CONFIG
 echo "QTS_PASSWORD=\"$qnap_password\"" >> $BOT_CONFIG
 
