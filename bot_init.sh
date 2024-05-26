e2a0457781697ac4860ab84b894968e9faaa6c0d
Oleg Primachenko <primachenko_oleg@outlook.com>
2024-05-26 12:48:58 +0700

    support schedule table

--- bot_init.sh
+++ bot_init.sh
@@ -8,7 +8,8 @@ read -p "Enter QNAP URL: " qnap_url
 read -p "Enter QNAP user: " qnap_user
 read -p "Enter QNAP password: " qnap_password
 
-read -p "Enter Google Tables table name: " google_table_name
+read -p "Enter Google Tables users table name: " google_users_table_name
+read -p "Enter Google Tables schedule table name: " google_schedule_table_name
 
 echo "SECRET=\"$bot_token\"" > $BOT_CONFIG
 echo "CHAT_ID=$techsupport_chat_id" >> $BOT_CONFIG
@@ -18,4 +19,5 @@ echo "QTS_HOST=\"$qnap_url\"" >> $BOT_CONFIG
 echo "QTS_USER=\"$qnap_user\"" >> $BOT_CONFIG
 echo "QTS_PASSWORD=\"$qnap_password\"" >> $BOT_CONFIG
 
-echo "GOOGLE_TABLE_NAME=\"$google_table_name\"" >> $BOT_CONFIG
+echo "GOOGLE_USERS_TABLE_NAME=\"$google_users_table_name\"" >> $BOT_CONFIG
+echo "GOOGLE_SCHEDULE_TABLE_NAME=\"$google_schedule_table_name\"" >> $BOT_CONFIG
