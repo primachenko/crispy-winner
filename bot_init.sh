c88224c311841ebb7cb46b07b4f009da83ec4fa4
v0lhv <v0lhv@ya.ru>
2025-03-02 23:16:46 +0700

    replace questions about QNAP to FTP

--- bot_init.sh
+++ bot_init.sh
@@ -4,9 +4,7 @@ read -p "Enter bot token: " bot_token
 read -p "Enter techsupport chat-id: " techsupport_chat_id
 read -p "Enter mgmt chat-id: " mgmt_chat_id
 
-read -p "Enter QNAP URL: " qnap_url
-read -p "Enter QNAP user: " qnap_user
-read -p "Enter QNAP password: " qnap_password
+read -p "Enter FTP URL: " ftp_url
 
 read -p "Enter Google Tables users table name: " google_users_table_name
 read -p "Enter Google Tables schedule table name: " google_schedule_table_name
@@ -15,9 +13,7 @@ echo "SECRET=\"$bot_token\"" > $BOT_CONFIG
 echo "CHAT_ID=$techsupport_chat_id" >> $BOT_CONFIG
 echo "MGMT_CHAT_ID=$mgmt_chat_id" >> $BOT_CONFIG
 
-echo "QTS_HOST=\"$qnap_url\"" >> $BOT_CONFIG
-echo "QTS_USER=\"$qnap_user\"" >> $BOT_CONFIG
-echo "QTS_PASSWORD=\"$qnap_password\"" >> $BOT_CONFIG
+echo "FTP_HOST=\"ftp_url\"" >> $BOT_CONFIG
 
 echo "GOOGLE_USERS_TABLE_NAME=\"$google_users_table_name\"" >> $BOT_CONFIG
 echo "GOOGLE_SCHEDULE_TABLE_NAME=\"$google_schedule_table_name\"" >> $BOT_CONFIG
